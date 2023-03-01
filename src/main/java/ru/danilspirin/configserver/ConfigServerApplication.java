925cf04a01169cb9154f1ecec1d3c9468cedf08b
Danil <danilkaspirin@gmail.com>
2023-03-01 10:06:59 +1000

    build: set native profile for server config and configure app

--- src/main/java/ru/danilspirin/configserver/ConfigServerApplication.java
+++ src/main/java/ru/danilspirin/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package ru.danilspirin.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // обьявляет данную службу службой Spring Cloud Config
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
